447c5f2ad90a4ab0ede19db209c286405536578b
timrinkerman <tim.rinkerman98@gmail.com>
2020-07-18 11:51:42 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,68 @@ class Song
     self.class.all << self
   end
 
+
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+def self.new_by_name(name)
+  song = self.new
+  song.name = name
+  song
+end
+
+def self.create_by_name(name)
+song = self.new
+song.name = name
+song.save
+song
+end
+
+def self.find_by_name(name)
+  @@all.find do |word|
+    word.name == name   
+  end
+end
+#can I call a method using self within a method 
+def self.find_or_create_by_name(name)
+  self.find_by_name(name) || self.create_by_name(name)
+
+end
+
+def self.alphabetical
+  @@all.sort_by { |name|  name.name }
+end
+
+def self.new_from_filename(string)
+  song = self.new
+  array = string.split(" - ")
+  array[1] = array[1].delete_suffix(".mp3")
+  
+  song.name = array[1]
+  song.artist_name = array[0]
+  
+  song  
+end
+
+#"Taylor Swift - Blank Space.mp3"
+def self.create_from_filename(string)
+song = self.new 
+array = string.split(" - ")
+  array[1] = array[1].delete_suffix(".mp3")
+  
+  song.name = array[1]
+  song.artist_name = array[0]  
+song.save
+
+
+end
+
+def self.destroy_all
+  @@all = []
+
+
+end
 end
